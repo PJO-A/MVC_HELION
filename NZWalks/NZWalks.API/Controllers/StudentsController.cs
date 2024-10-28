8d024b824a58ec93b40014fb09fb4cd1e672e203
PJO-A <info@pjo-automation.co.uk>
2024-10-27 22:49:45 -0700

    2 Create new ASP.NET

---
+++ NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //this url will be:     https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: this url will be:       https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Emily", "David" };
+            return Ok(studentNames);
+        }
+
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Data/NZwalksDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZwalksDbContext :DbContext
+    {
+        // class constructor
+        public NZwalksDbContext(DbContextOptions dbContectOptions): base(dbContectOptions)
+        {
+            
+        }
+
+        //DBSETS - properties of DBContext class, whcih represents collection of entities in database
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+
+
+    }
+      
+}
